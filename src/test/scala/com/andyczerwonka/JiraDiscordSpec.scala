eb9cbe89fab98b28750140f3d71a477fc93d40a6
Andy Czerwonka <andy.czerwonka@gmail.com>
2017-09-22 13:57:28 -0600

    #1: delete the files the came with the template

---
+++ src/test/scala/com/andyczerwonka/JiraDiscordSpec.scala
@@ -0,0 +1,9 @@
+package com.andyczerwonka
+
+import org.scalatest._
+
+class JiraDiscordSpec extends FlatSpec with Matchers {
+  "The dummy call" should "say hello" in {
+    "Fred" shouldEqual "Fred"
+  }
+}
